db14a7e84d561c176f08fd982f4e9e4cf0d2505d
alexagold1 <526agold@frhsd.com>
2025-09-10 10:39:02 -0400

    added navbar and modal

---
+++ script.js
@@ -0,0 +1,8 @@
+var video = document.getElementById("myVideo");
+
+var btn = document.getElementById("myBtn");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = new bootstrap.Modal(document.getElementById("syracuseModal"));
+  modal.show();
+});

5bd5a4fbe29cfafdb7efb60b19c095a76d5ac77c
alexagold1 <526agold@frhsd.com>
2025-09-11 11:49:23 -0400

    added apply now button

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 var video = document.getElementById("myVideo");
 
 var btn = document.getElementById("myBtn");
-
-document.addEventListener("DOMContentLoaded", () => {
-  const modal = new bootstrap.Modal(document.getElementById("syracuseModal"));
-  modal.show();
-});
